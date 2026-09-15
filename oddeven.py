e9af5ef539593f6d0c035ebbf9aa8ccdbb2863d5
ZaneStephens111 <stephezm@mail.uc.edu>
2026-09-15 12:09:44 +0200

    Added Odd/Even

---
+++ oddeven.py
@@ -0,0 +1,5 @@
+user_input = str(input("Enter a word:"))
+if len(user_input) % 2 == 0:
+    print("The word has an even number of characters.")
+else:
+    print("The word has an odd number of characters.")
