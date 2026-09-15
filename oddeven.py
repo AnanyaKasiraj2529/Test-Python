5abc5a2734ab857bb0f1093cf38cd600ca64ca0c
Abigail Verholen <abigail.verholen.001@student.uni.lu>
2026-09-15 12:14:42 +0200

    changed to function

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,5 @@
-user_input = str(input("Enter a word:"))
-if len(user_input) % 2 == 0:
-    print("The word has an even number of characters.")
-else:
-    print("The word has an odd number of characters.")
+def check_odd(word):
+    if len(word) % 2 == 0:
+        print("No")
+    else:
+        print("Yes")
